e79b84aee831dc246c3daad4e1cd767080985bce
darinchakalov <darinchakalov@hotmail.com>
2022-09-15 21:42:27 +0300

    working shift time calculator

---
+++ WorkHoursNotifications/main.py
@@ -0,0 +1,32 @@
+from xml.etree.ElementTree import tostring
+from plyer import notification
+from datetime import datetime
+from datetime import time
+
+# start = datetime.strptime("08:30:00", "%H:%M:%S")
+start = time.fromisoformat('08:00:00')
+now = datetime.now().strftime("%H:%M:%S")
+
+
+start_time = start.strftime("%H:%M:%S")
+
+
+def time_formatter(start_time, end_time):
+    start_hours, start_minuts, start_seconds = start_time.split(':')
+    end_hours, end_minuts, end_seconds = end_time.split(':')
+
+    # print(int(end_hours)-int(start_hours))
+    return f'{(int(end_hours)-int(start_hours))}:{(int(end_minuts)-int(start_minuts))}'
+
+def percentage_calculator():
+    hours_passed, minutes_passed = time_formatter(start_time, now).split(":")
+    time_passed = float(f'{hours_passed}.{minutes_passed}')
+    return time_passed / 8.0 * 100
+    
+
+notification.notify(
+    title=f'{time_formatter(start_time, now)} from the beginning of the shift',
+    message=f'Percentage passed: {percentage_calculator()}',
+    app_name='Here is the application name',
+    # app_icon='/icon.png'
+)
